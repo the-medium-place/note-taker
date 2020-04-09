f0701c5f1df0d5dca21cc662d9c7bbdb598409fb
Zac Stowell <zgstowell@gmail.com>
2020-04-09 16:08:19 -0700

    delete button almost

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,13 +5,14 @@ const path = require("path");
 const fs = require("fs");
 const util = require("util");
 
-// const uuidv1 = require('uuid/v1');
+const uuidv1 = require('uuid/v1');
+
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 
-let notesUpdate = "";
+let notesUpdate;
 
 router.get("/api/notes", (req, res) => {
 
@@ -31,23 +32,56 @@ router.get("/api/notes", (req, res) => {
 
 router.post("/api/notes", (req, res) => {
     const newNote = req.body;
-    // let notesUpdate = "";
+    newNote.id = uuidv1();
+    console.log(req.body);
 
-
+    // read db.json for existing data
     readFileAsync(path.join(__dirname, "../db/db.json"), "utf8")
-        .then(data => {
+    .then(data => {
+        // then parse into readable array of objects
             const parseData = JSON.parse(data);
+        // add new note to parsed data array
             const combineNotes = [...parseData, newNote];
-          
+        // set global variable equal to string of updated data
+            notesUpdate = JSON.stringify(combineNotes);
 
-            // console.log("before " + notesUpdate);
-            notesUpdate += JSON.stringify(combineNotes);
-            // console.log("after " + notesUpdate);
-            // notesUpdate += combineNotes;
         })
+        // then write updated string to db.json
         .then(() => writeFileAsync(path.join(__dirname, "../db/db.json"), notesUpdate))
-        .then(() => console.log("after then " + notesUpdate))
+        // then display JSON info
+        .then(() => res.json(notesUpdate))
         .catch(err => !err ? console.log("success") : console.log(err));
+
+})
+
+
+
+router.delete("/api/notes/:id", (req,res) => {
+    const choice = req.params.id;
+    fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
+
+        if (err) throw err;
+
+        const userData = [];
+
+        data = JSON.parse(data);
+        console.log(data);
+
+        for(i=0; i<data.length; i++){
+            if (data[i].id !== req.params.id){
+                console.log(data[i])
+                userData.push(data[i]);
+            }
+        }
+        console.log("after for loop" + userData);
+        
+
+
+        // data = data.filter(note => note.id !== req.params.id)
+
+    })
+
+
 
 })
 
