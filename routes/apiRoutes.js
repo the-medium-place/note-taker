6528f292a8971b217c8d6e1c40f7d49f3cc74f04
Zac Stowell <zgstowell@gmail.com>
2020-04-09 20:29:45 -0700

    cleaned up code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,16 +3,8 @@ const express = require('express');
 const router = express.Router();
 const path = require("path");
 const fs = require("fs");
-const util = require("util");
 
 const uuidv1 = require('uuid/v1');
-
-
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
-
-
-let notesUpdate;
 
 router.get("/api/notes", (req, res) => {
 
@@ -29,68 +21,54 @@ router.get("/api/notes", (req, res) => {
 
 });
 
-
 router.post("/api/notes", (req, res) => {
     const newNote = req.body;
     newNote.id = uuidv1();
-    console.log(req.body);
 
-    // read db.json for existing data
-    readFileAsync(path.join(__dirname, "../db/db.json"), "utf8")
-    .then(data => {
-        // then parse into readable array of objects
-            const parseData = JSON.parse(data);
-        // add new note to parsed data array
-            const combineNotes = [...parseData, newNote];
-        // set global variable equal to string of updated data
-            notesUpdate = JSON.stringify(combineNotes);
+    // read db file (string)
+    fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
+        // then parse data to array of objects
+        const parseData = JSON.parse(data);
 
+        // add new note to parsed data array and stringify
+        const combineNotes = [...parseData, newNote];
+        const userData = JSON.stringify(combineNotes);
+     
+        // write combineNotes string to db
+        fs.writeFile(path.join(__dirname, "../db/db.json"), userData, (error, response) => {
+            if (error) throw error;
+            res.json({ ok: true });
         })
-        // then write updated string to db.json
-        .then(() => writeFileAsync(path.join(__dirname, "../db/db.json"), notesUpdate))
-        // then display JSON info
-        .then(() => res.json(notesUpdate))
-        .catch(err => !err ? console.log("success") : console.log(err));
+ 
+    })
 
 })
 
-
-
 router.delete("/api/notes/:id", (req,res) => {
-    // const choice = req.params.id;
+    
     fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
-
         if (err) throw err;
 
         let userData = [];
 
+        // get data array of objects
         data = JSON.parse(data);
-        console.log(data);
 
+        // loop through data array for NON-matching and build new array  
         for(i=0; i<data.length; i++){
             if (data[i].id !== req.params.id){
-                console.log(data[i])
                 userData.push(data[i]);
             }
         }
-        console.log("after for loop" + userData);
+        // convert new array to string
         userData = JSON.stringify(userData);
 
+        // write new string to db
         fs.writeFile(path.join(__dirname, "../db/db.json"), userData, (err, response) => {
             if (err) throw err;
             res.json({ ok: true });
         })
-        
-
-
-        // data = data.filter(note => note.id !== req.params.id)
-
     })
-
-
-
 })
-
-
 
 module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -14,5 +14,4 @@ router.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-
 module.exports = router;
